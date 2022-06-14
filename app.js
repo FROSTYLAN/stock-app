02efc00cd601f68be12d7c850d6be62f96255aa0
FROSTYLAN <castillo089frosty@gmail.com>
2022-06-14 14:18:53 -0500

    starting repo

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("petición recibida");
+
+  res.send("Hola mundo!");
+});
+
+app.listen(4000, () => console.log("Servidor escuchando en el puerto 4000"));
